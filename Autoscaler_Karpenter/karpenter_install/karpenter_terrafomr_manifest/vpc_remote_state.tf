b21cb98cc43df057848bca562c98a512cb28130c
Naveedali <navedalisayyed@gmail.com>
2026-05-03 08:39:48 +0530

    Karpenter for Cluster Autoscaling

---
+++ Autoscaler_Karpenter/karpenter_install/karpenter_terrafomr_manifest/vpc_remote_state.tf
@@ -0,0 +1,40 @@
+# --------------------------------------------------------------------
+# Reference the Remote State from VPC Project
+# --------------------------------------------------------------------
+
+data "terraform_remote_state" "vpc"{
+    backend = "s3"
+
+    config = {
+       bucket = "tfstate-dev-us-east-1-3mnrih"
+       key    = "vpc/dev/terraform.tfstate"
+       region = var.aws_region
+}
+
+}
+
+# --------------------------------------------------------------------
+# Output the VPC ID from the remote VPC state
+# --------------------------------------------------------------------
+
+output "vpc_id" {
+    value = data.terraform_remote_state.vpc.outputs.vpc_id
+  
+}
+
+# --------------------------------------------------------------------
+# Output the list of private subnets from the VPC
+# --------------------------------------------------------------------
+
+output "private_subnet_ids" {
+    value = data.terraform_remote_state.vpc.outputs.private_subnet_ids
+  
+}
+
+# --------------------------------------------------------------------
+# Output the list of public subnets from the VPC
+# --------------------------------------------------------------------
+
+output "public_subnet_ids" {
+  value = data.terraform_remote_state.vpc.outputs.public_subnet_ids
+}
